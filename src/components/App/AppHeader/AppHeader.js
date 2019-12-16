121d31fe180a0a6a180904fe425fd100210039bd
Riv <rivian@protonmail.com>
2019-12-16 19:47:09 +0100

    Add app header compomemnt

---
+++ src/components/App/AppHeader/AppHeader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './AppHeader.css';
+
+const AppHeader = (props) => {
+    return (
+    <div className="header">
+        <h1>Todo, todo, todo-todo-todo-todo todooooooooo</h1>
+    </div>
+    );
+}
+
+export default AppHeader;
